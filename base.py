29793a8203cc93fa40a5ddce027ca76292973763
Gauss243J <78795548+Gauss243J@users.noreply.github.com>
2024-08-06 14:02:00 +0200

    Update base.py
    add file

--- base.py
+++ base.py
@@ -1,10 +1,10 @@
 import openai
 import streamlit as st
-from langchain_community.llms import OpenAI
-from langchain_community.chat_models import ChatOpenAI
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
